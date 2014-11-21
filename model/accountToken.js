4f9ae7e899608eec5bdec4a51cd197c1cf9fe763
soomtong <soomtong@gmail.com>
2014-11-22 00:13:12 +0900

    `modify` password update and ip information to access token

--- model/accountToken.js
+++ model/accountToken.js
@@ -4,6 +4,7 @@ var accountTokenSchema = new mongoose.Schema({
     haroo_id: { type: String, index: true },
     access_token: { type: String, unique: true, index: true },
     access_host: String,
+    access_ip: String,
     login_expire: String,
     created_at: Date
 });
