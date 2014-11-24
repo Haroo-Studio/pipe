b5c39a79d249988333532be4210e3e14b1c72448
soomtong <soomtong@gmail.com>
2014-11-25 03:19:18 +0900

    `add` some test code

--- test/account.js
+++ test/account.js
@@ -1,5 +1,67 @@
-exports.testSomething1 = function(test){
-    test.expect(1);
-    test.ok(true, "this assertion should pass");
-    test.done();
+var mongoose = require('mongoose');
+var database = require('../config/database');
+
+var Account = require('../lib/account');
+
+var params = {
+    email: "test@email.com",
+    nickname: "new tester",
+    password: "password",
+    accessHost: "test.local",
+    accessIP: "100.9.9.9",
+    haroo_id: '',
+    database: database,
+    result: {}
+};
+
+exports.setUp = function (callback) {
+    mongoose.connect(database['mongo'].host);
+    mongoose.connection.on('error', function () {
+        console.error('MongoDB Connection Error. Make sure MongoDB is running.');
+    });
+
+    var AccountModel = require('../model/account');
+
+    AccountModel.remove({email: params.email}, function (err, affected) {
+        //console.log(err ? err: "", "remove documents before tests:" ,affected);
+
+        callback();
+    });
+};
+
+exports.createByEmail = function (t) {
+    t.expect(2);
+
+    Account.createByEmail(params, function (result) {
+        t.equal(result.email, params.email);
+        t.equal(result.db_host, params.database.couch.host);
+        t.done();
+    });
+};
+
+
+exports.findByHarooID = function (t) {
+    t.expect(2);
+
+    Account.createByEmail(params, function (result) {
+        params['accessToken'] = result.access_token;
+        params['haroo_id'] = result.haroo_id;
+
+        Account.findByHarooID(params, function (result2) {
+            t.equal(result.access_token, result2.access_token);
+            t.equal(result.haroo_id, result2.haroo_id);
+            t.done();
+        });
+    });
+};
+
+exports.tearDown = function (callback) {
+    var AccountModel = require('../model/account');
+
+    AccountModel.remove({email: params.email}, function (err, affected) {
+        mongoose.disconnect();
+        //console.log(err ? err: "", "remove documents after tests:" ,affected);
+
+        callback();
+    });
 };
--- test/common.js
+++ test/common.js
@@ -1,13 +1,14 @@
 var CommonUtil = require('../lib/common');
 
-exports.getToday = function (test) {
-    test.expect(1);
-    test.equal(CommonUtil.getToday(), new Date().toISOString().slice(0, 10));
-    test.done();
+exports.getToday = function (t) {
+    t.expect(1);
+    t.equal(CommonUtil.getToday(), new Date().toISOString().slice(0, 10));
+    t.done();
 };
 
-exports.setAccountToClient = function (test) {
-    test.expect(1);
+exports.setAccountToClient = function (t) {
+    t.expect(1);
+
     var userData = {
         "login_expire" : "1417550749401",
         "access_token" : "6c062089-ad66-4110-9f8f-187f67b57c01",
@@ -18,13 +19,19 @@ exports.setAccountToClient = function (test) {
         },
         "db_host": "db1.haroopress.com"
     };
+    var tokenData = {
+        "access_ip" : "127.0.0.1",
+        "access_host" : "sven-mac-pro1",
+        "access_token" : "6d3ca5ff-6398-47d3-be49-49e18d8124e7",
+        "haroo_id" : "b4c4ae0692b435427b671649ea30848e7",
+        "login_expire" : "1417881428672"
+    };
     var code = {
-            code: 0,
-            msg: {
-                en: "message english"
-            }
-        };
-
+        code: 0,
+        msg: {
+            en: "message english"
+        }
+    };
     var result = {
         code: 0,
         msg: {
@@ -32,13 +39,41 @@ exports.setAccountToClient = function (test) {
         },
         "email" : "soomtong@gmail.com",
         "haroo_id" : "b4c4ae0692b435427b671649ea30848e7",
-        "access_token" : "6c062089-ad66-4110-9f8f-187f67b57c01",
-        "login_expire" : "1417550749401",
         "profile" : {
             "nickname" : "soomtong"
         },
-        "db_host": "db1.haroopress.com"
+        "db_host": "db1.haroopress.com",
+        "access_host": "sven-mac-pro1",
+        "access_token": "6d3ca5ff-6398-47d3-be49-49e18d8124e7",
+        "login_expire": "1417881428672"
     };
-    test.deepEqual(CommonUtil.setAccountToClient(code, userData), result);
-    test.done();
+    t.deepEqual(CommonUtil.setAccountToClient(code, userData, tokenData), result);
+    t.done();
+};
+
+exports.isThisTokenExpired = function (t) {
+    t.expect(2);
+
+    var HOUR = 3600000;
+    var DAY = HOUR * 24;
+
+    var validTokenData = {
+        "login_expire" : Date.now() + DAY
+    };
+    var invalidTokenData = {
+        "login_expire" : Date.now() - HOUR
+    };
+
+    t.ok(!CommonUtil.isThisTokenExpired(validTokenData));
+    t.ok(CommonUtil.isThisTokenExpired(invalidTokenData));
+    t.done();
+};
+
+exports.makeHarooID = function (t) {
+    t.expect(1);
+    var email = 'soomtong@gmail.com';
+    var validHarooID = CommonUtil.initHarooID(email);
+
+    t.equal(validHarooID, CommonUtil.initHarooID(email));
+    t.done();
 };
