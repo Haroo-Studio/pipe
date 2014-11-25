59b1f8f686f6f6bb40be13084d1c3132f9b7fc7b
soomtong <soomtong@gmail.com>
2014-11-25 12:56:32 +0900

    `modify` separate configuration to core

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-var database = require('./config/database');
-var mongoose = require('mongoose');
-var passportLoader = require('./lib/passport');
-
-mongoose.connect(database['mongo'].host);
-mongoose.connection.on('error', function () {
-    console.error('MongoDB Connection Error. Make sure MongoDB is running.');
-});
-
 module.exports = {
     HarooCode: require('./lib/code'),
+    MongoInit: require('./lib/mongo'),
     CommonUtil: require('./lib/common'),
     Account: require('./lib/account'),
     AccountToken: require('./lib/accountToken'),
-    Passport: require('passport')
+    Passport: require('passport'),
+    PassportConfig: require('./lib/passport')
 };
