133eb84c32336c92b4b2583dfcdd930ccd72592d
soomtong <soomtong@gmail.com>
2014-11-25 14:52:06 +0900

    `add` account log lib

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
     MongoInit: require('./lib/mongo'),
     CommonUtil: require('./lib/common'),
     Account: require('./lib/account'),
+    AccountLog: require('./lib/accountLog'),
     AccountToken: require('./lib/accountToken'),
     Passport: require('passport'),
     PassportConfig: require('./lib/passport')
