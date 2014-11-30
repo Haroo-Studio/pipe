9638a89dcc27ad1b19c99df5b96b1a81b9d62964
soomtong <soomtong@gmail.com>
2014-11-30 19:17:47 +0900

    `add` public docs model

---
+++ model/publicDocument.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var publicDocSchema = new mongoose.Schema({
+    release_date: { type: String, index: true },
+    counter: { type: Number, index: true },
+    haroo_id: { type: String, index: true },
+    document_id: { type: String, index: true },
+    public: Boolean,
+    subscribe_user: Array
+});
+
+module.exports = mongoose.model('public_document', publicDocSchema);
