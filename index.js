b1be7b6a713fb5d53c69686f95318e69e9343674
soomtong <soomtong@gmail.com>
2014-11-30 19:16:48 +0900

    `add` couch to nano bind

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 module.exports = {
     HarooCode: require('./lib/code'),
     MongoInit: require('./lib/mongo'),
+    CouchConnect: require('./lib/couch'),
     CommonUtil: require('./lib/common'),
     Account: require('./lib/account'),
     AccountLog: require('./lib/accountLog'),
     AccountToken: require('./lib/accountToken'),
+    Document: require('./lib/document'),
     Passport: require('passport'),
     PassportConfig: require('./lib/passport')
 };
